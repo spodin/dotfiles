fc627ef386163b699b7c3e6e977a22e142e47a2a
Vasiliy Spodin <v@spodin.com>
2020-01-25 18:28:11 +0200

    Use HOME environment variable with curly braces

--- install.sh
+++ install.sh
@@ -10,20 +10,20 @@ DOTFILES="$HOME/.dotfiles"
 
 install() {
   # Create symlinks
-  ln -fs ${DOTFILES}/home/.gitconfig $HOME/.gitconfig
-  ln -fs ${DOTFILES}/home/.gitignore_global $HOME/.gitignore_global
-  ln -fs ${DOTFILES}/home/.hyper.js $HOME/.hyper.js
-  ln -fs ${DOTFILES}/home/.zshrc $HOME/.zshrc
+  ln -fs ${DOTFILES}/home/.gitconfig ${HOME}/.gitconfig
+  ln -fs ${DOTFILES}/home/.gitignore_global ${HOME}/.gitignore_global
+  ln -fs ${DOTFILES}/home/.hyper.js ${HOME}/.hyper.js
+  ln -fs ${DOTFILES}/home/.zshrc ${HOME}/.zshrc
 
   # Create machine specific configuration files from templates
-  cp -n ${DOTFILES}/templates/.env $HOME/.env
-  cp -n ${DOTFILES}/templates/.gitlocal $HOME/.gitlocal
-  cp -n ${DOTFILES}/templates/.gitmessage $HOME/.gitmessage
-  cp -n ${DOTFILES}/templates/.warprc $HOME/.warprc
-  cp -n ${DOTFILES}/templates/ssh/config $HOME/.ssh/config
+  cp -n ${DOTFILES}/templates/.env ${HOME}/.env
+  cp -n ${DOTFILES}/templates/.gitlocal ${HOME}/.gitlocal
+  cp -n ${DOTFILES}/templates/.gitmessage ${HOME}/.gitmessage
+  cp -n ${DOTFILES}/templates/.warprc ${HOME}/.warprc
+  cp -n ${DOTFILES}/templates/ssh/config ${HOME}/.ssh/config
 
   # Symlink embedded Oh My Zsh
-  ln -fs ${DOTFILES}/zsh/ohmyzsh $HOME/.oh-my-zsh
+  ln -fs ${DOTFILES}/zsh/ohmyzsh ${HOME}/.oh-my-zsh
 
   echo "Completed successfully."
 }
