1d00888f0ac265ec6010bb4872ba1985546a60e6
Vasiliy Spodin <v@spodin.com>
2020-01-26 00:52:39 +0200

    Improve

--- configure_zsh.sh
+++ configure_zsh.sh
@@ -6,12 +6,22 @@
 
 ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
 
-# Spaceship theme (https://denysdovhan.com/spaceship-prompt)
-git clone https://github.com/denysdovhan/spaceship-prompt.git ${ZSH_CUSTOM}/themes/spaceship-prompt
-ln -s ${ZSH_CUSTOM}/themes/spaceship-prompt/spaceship.zsh-theme ${ZSH_CUSTOM}/themes/spaceship.zsh-theme
+install_theme() {
+  # Spaceship theme (https://denysdovhan.com/spaceship-prompt)
+  git clone https://github.com/denysdovhan/spaceship-prompt.git ${ZSH_CUSTOM}/themes/spaceship-prompt
+  ln -s ${ZSH_CUSTOM}/themes/spaceship-prompt/spaceship.zsh-theme ${ZSH_CUSTOM}/themes/spaceship.zsh-theme
+}
 
-# Autosuggestions plugin
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM}/plugins/zsh-autosuggestions
+install_plugin() {
+  url="$1"
+  name="$(basename "$url" .git)"
+  git clone ${url} ${ZSH_CUSTOM}/plugins/${name}
+}
 
-# Syntax-highlighting plugin
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting
+configure() {
+  install_theme
+  install_plugin "https://github.com/zsh-users/zsh-autosuggestions.git"
+  install_plugin "https://github.com/zsh-users/zsh-syntax-highlighting.git"
+}
+
+configure
