144eae4253cc9c36dd0dccf96cad09672c5a7764
Vasiliy Spodin <v@spodin.com>
2020-02-17 20:48:20 +0200

    Add extra Zsh configuration and customizations

---
+++ zsh/config.zsh
@@ -0,0 +1,11 @@
+#
+# Extra Zsh configuration and customizations.
+#
+
+# Don't record duplicates in history
+setopt HIST_EXPIRE_DUPS_FIRST
+setopt HIST_IGNORE_ALL_DUPS
+setopt HIST_IGNORE_DUPS
+setopt HIST_IGNORE_SPACE
+setopt HIST_REDUCE_BLANKS
+setopt HIST_VERIFY
