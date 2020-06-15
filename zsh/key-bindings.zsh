24645563d51eae75e70f4fc8876ddc1a7691a93a
Vasiliy Spodin <v@spodin.com>
2020-06-15 16:22:11 +0300

    Add bindings for `Home` and `End` keys

--- zsh/key-bindings.zsh
+++ zsh/key-bindings.zsh
@@ -3,6 +3,8 @@
 # Use `xxd` command for getting key codes.
 #
 
-bindkey '^[(' kill-word         # [Fn-Option-Delete] - delete forward word (macOS)
-bindkey '^[[3;5~' kill-word     # [Ctrl-Delete] - delete forward word
-bindkey '^H' backward-kill-word # [Ctrl-Backspace] - delete backward word
+bindkey '^[(' kill-word              # [Fn-Option-Delete] - delete forward word (macOS)
+bindkey '^[[3;5~' kill-word          # [Ctrl-Delete] - delete forward word
+bindkey '^H' backward-kill-word      # [Ctrl-Backspace] - delete backward word
+bindkey "\033[1~" beginning-of-line  # [Home] - go to beginning
+bindkey "\033[4~" end-of-line        # [End] - go to end
