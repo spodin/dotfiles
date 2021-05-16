b5d68afc722cfe77b32f428ce53a65c8fbf2a68d
Vasiliy Spodin <v@spodin.com>
2021-05-16 12:58:10 +0300

    Merge `key-bindings.zsh` into `config.zsh`
    This will reduce number of files to load on startup.

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -9,3 +9,14 @@ setopt HIST_IGNORE_DUPS
 setopt HIST_IGNORE_SPACE
 setopt HIST_REDUCE_BLANKS
 setopt HIST_VERIFY
+
+#
+# Key bindings.
+# Use `xxd` command for getting key codes.
+#
+
+bindkey '^[(' kill-word              # [Fn-Option-Delete] - delete forward word (macOS)
+bindkey '^[[3;5~' kill-word          # [Ctrl-Delete] - delete forward word
+bindkey '^H' backward-kill-word      # [Ctrl-Backspace] - delete backward word
+bindkey "\033[1~" beginning-of-line  # [Home] - go to beginning
+bindkey "\033[4~" end-of-line        # [End] - go to end
