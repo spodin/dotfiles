d0c04ace84a6b8abba2136f71e9be7ca4ad1ecce
Vasiliy Spodin <v@spodin.com>
2021-05-17 16:34:28 +0300

    Increase history size
    See: https://unix.stackexchange.com/questions/273861/unlimited-history-in-zsh

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -2,6 +2,10 @@
 # Extra Zsh configuration and customizations.
 #
 
+export HISTSIZE=10000000
+export SAVEHIST=10000000
+export HISTFILE="$HOME/.zsh_history"
+
 setopt HIST_EXPIRE_DUPS_FIRST        # Expire duplicate entries first when trimming history
 setopt HIST_IGNORE_ALL_DUPS          # Delete old recorded entry if new entry is a duplicate
 setopt HIST_IGNORE_DUPS              # Don't record an entry that was just recorded again
